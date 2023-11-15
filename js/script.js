8ea18ff5242b1971ae4d65490ccc1b683d78aac7
Junior <129011987+JuniorCesarMarques@users.noreply.github.com>
2023-11-14 23:25:00 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,34 @@
 const navButtons = document.querySelectorAll(".nav_button");
 navButtons[0].style.backgroundColor = "var(--cor-primaria)";
+const barraDeProgresso = document.querySelector("#barra_progresso");
+const timer = document.querySelector("#timer");
+const containerBarProgress = document.querySelector("#contagem_regressiva");
+const girandoText = document.querySelector("#girando_text");
+let timerStart = "15";
+
+
+
+ let intervalId = setInterval(function timerAnimation() {
+    timer.innerText = "Girando em " + timerStart--;
+}, 1000)
+
+setTimeout(() => {
+    clearInterval(intervalId);
+    containerBarProgress.style.display = "none";
+    girandoText.innerText = "Girando...";
+}, 17000);
+
+
+
+function animationBar() {
+    barraDeProgresso.style.animation = "animationBar 15s";
+
+};
+
+animationBar();
+
+
+
 
 navButtons.forEach((button, index) => {
     button.addEventListener("click", () => {
